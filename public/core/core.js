934a68a2a4faff60be9fba50b64ad94d8b074e38
chai41104 <chai41104@hotmail.com>
2017-04-30 13:30:46 +0100

    update

--- public/core/core.js
+++ public/core/core.js
@@ -1,5 +1,15 @@
-(function() {
-  document.querySelector("a-box").addEventListener("click", function(evt) {
-    document.querySelector("a-box").setAttribute("visible", false)
-  })
-})();
+// (function() {
+//   document.querySelector("a-box").addEventListener("click", function(evt) {
+//     document.querySelector("a-box").setAttribute("visible", false)
+//   })
+// })();
+
+var fadeValue = 0.1;
+
+setInterval(function(){
+	var opacity = document.getElementById('opacity');
+	var value = opacity.getAttribute('text', 'opacity') + fadeValue;
+	if(value <= 0) fadeValue = 0.1;
+	else if(value >= 1) fadeValue = -0.1; 
+	opacity.setAttribute('text', 'opacity', vallue);
+}, 100);
